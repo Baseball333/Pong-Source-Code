bb3fbdcca89b351919af49330421d398503f1dee
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-11-23 20:48:38 -0800

    Second commit
    Import libraries, specify graphical shell and write score.

--- pong1.py
+++ pong1.py
@@ -1,2 +1,15 @@
 # Pong variant
 # Commit 1
+
+import turtle
+import os
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=400)
+
+# Score
+score_a = 0
+score_a = 0
+
